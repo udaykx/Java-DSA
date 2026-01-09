4b4075264c2ed5cc21c2eb9c242201768c6f8fa5
udaykx <udaykumar2600@gmail.com>
2026-01-09 22:59:02 +0530

    start

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int rollno = 64;
+//        char letter = 'U';
+//        float marks = 98.67f;
+//        double largeDecimalNumbers = 4567747438.34434;
+//        long largeInteger = 435345435345L;
+//        boolean check = false;
+//        //string is not primitive
+//        String name = "Uday";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some input: ");
+        int rollno = input.nextInt();
+        System.out.println("Your Roll number is: " + rollno);
+    }
+}
